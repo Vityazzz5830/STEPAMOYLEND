6e8ee8234245f4a5db3eadd9a9319a05eebbceb4
Vityazzz5830 <121909224+Vityazzz5830@users.noreply.github.com>
2024-03-10 00:11:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,312 @@
+let yandexmap = document.getElementsByClassName('yandexmap');
+
+if (yandexmap[0]) {
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('yandexmap', {
+        center: [55.712650, 37.605484],
+        zoom: 14,
+        controls: []
+    }, 
+
+    {
+      searchControlProvider: 'yandex#search'
+    }),
+
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemark1 = new ymaps.Placemark([55.718500, 37.605125], {}, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/mapsec_baloon.svg',
+      // Размеры метки.
+      iconImageSize: [30, 42],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    })
+
+    myPlacemark2 = new ymaps.Placemark([55.709807, 37.590874], {}, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/mapsec_baloon.svg',
+      // Размеры метки.
+      iconImageSize: [30, 42],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    })
+
+    myPlacemark3 = new ymaps.Placemark([55.721125, 37.576787], {}, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/mapsec_baloon.svg',
+      // Размеры метки.
+      iconImageSize: [30, 42],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    })
+
+    myPlacemark4 = new ymaps.Placemark([55.709809, 37.611712], {}, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/mapsec_baloon.svg',
+      // Размеры метки.
+      iconImageSize: [30, 42],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    })
+
+    myPlacemark5 = new ymaps.Placemark([55.703516, 37.619438], {}, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/mapsec_baloon.svg',
+      // Размеры метки.
+      iconImageSize: [30, 42],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    })
+
+    myMap.geoObjects
+        .add(myPlacemark1)
+        .add(myPlacemark2)
+        .add(myPlacemark3)
+        .add(myPlacemark4)
+        .add(myPlacemark5);
+});
+
+}
+
+
+
+window.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call( document.querySelectorAll('input[data-tel-input]'), function(input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          new_value = matrix.replace(/[_\d]/g, function(a) {
+              return i < val.length ? val.charAt(i++) : a
+          });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+          i < 5 && (i = 3);
+          new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function(a) {
+              return "\\d{1," + a.length + "}"
+          }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        this.value = new_value;
+      }
+      if (event.type == "blur" && this.value.length < 5) {
+        this.value = "";
+      }
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+  });
+});
+
+var nameInputs = document.querySelectorAll('input[data-name-input]');
+
+$(nameInputs).on('keypress', function() {
+    var that = this;
+
+    setTimeout(function() {
+        var res = /[^аА-яЯ]/g.exec(that.value);
+        console.log(res);
+        that.value = that.value.replace(res, '');
+    }, 0);
+});
+
+
+var emailInputs = document.querySelectorAll('input[data-email-input]');
+
+$(emailInputs).on('keypress', function() {
+    var that = this;
+
+    setTimeout(function() {
+        var res = /[^aA-zZ1234567890@.?!%&()><*-_]/g.exec(that.value);
+        console.log(res);
+        that.value = that.value.replace(res, '');
+    }, 0);
+});
+
+
+
+
+
+
+
+
+/*--------------BURGER MENU-----------------------*/
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const close_popup = document.querySelector("#close_popup");
+
+const body = document.body;
+
+const btn1 = document.getElementsByClassName('header_content-wrap-navmenu-list-item-1')[0];
+const btn2 = document.getElementsByClassName('header_content-wrap-navmenu-list-item-2')[0];
+const btn3 = document.getElementsByClassName('header_content-wrap-navmenu-list-item-3')[0];
+const btn4 = document.getElementsByClassName('header_content-wrap-navmenu-list-item-4')[0];
+const btn5 = document.getElementsByClassName('header_content-wrap-navmenu-list-item-5')[0];        
+const btn6 = document.getElementsByClassName('header_content-wrap-navmenu-list-item-6')[0];
+const btn7 = document.getElementsByClassName('header_content-wrap-navmenu-list-item-7')[0]; 
+
+hamb.addEventListener("click", hambHandler);
+close_popup.addEventListener("click", hambHandler);
+
+
+function hambHandler(e) {
+  e.preventDefault();
+
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+}
+
+
+if(btn1) {
+  btn1.addEventListener('click', function() {
+      console.log(111);
+     popup.classList.toggle("open");
+     body.classList.toggle("noscroll");
+  });
+}
+
+if(btn2) {
+  btn2.addEventListener('click', function() {
+      console.log(111);
+     popup.classList.toggle("open");
+     body.classList.toggle("noscroll");
+  });
+}
+
+if(btn3) {
+  btn3.addEventListener('click', function() {
+      console.log(111);
+     popup.classList.toggle("open");
+     body.classList.toggle("noscroll");
+  });
+}
+
+if(btn4) {
+  btn4.addEventListener('click', function() {
+      console.log(111);
+     popup.classList.toggle("open");
+     body.classList.toggle("noscroll");
+  });
+}
+
+if(btn5) {
+  btn5.addEventListener('click', function() {
+      console.log(111);
+     popup.classList.toggle("open");
+     body.classList.toggle("noscroll");
+  });
+}
+
+if(btn6) {
+  btn6.addEventListener('click', function() {
+      console.log(111);
+     popup.classList.toggle("open");
+     body.classList.toggle("noscroll");
+  });
+}
+
+if(btn7) {
+  btn7.addEventListener('click', function() {
+      console.log(111);
+     popup.classList.toggle("open");
+     body.classList.toggle("noscroll");
+  });
+}
+
+
+
+
+window.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call( document.querySelectorAll('input[data-tel-input]'), function(input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          new_value = matrix.replace(/[_\d]/g, function(a) {
+              return i < val.length ? val.charAt(i++) : a
+          });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+          i < 5 && (i = 3);
+          new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function(a) {
+              return "\\d{1," + a.length + "}"
+          }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        this.value = new_value;
+      }
+      if (event.type == "blur" && this.value.length < 5) {
+        this.value = "";
+      }
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+  });
+});
+
+var nameInputs = document.querySelectorAll('input[data-name-input]');
+
+$(nameInputs).on('keypress', function() {
+    var that = this;
+
+    setTimeout(function() {
+        var res = /[^аА-яЯ]/g.exec(that.value);
+        console.log(res);
+        that.value = that.value.replace(res, '');
+    }, 0);
+});
+
+
+
+
+
+
+
